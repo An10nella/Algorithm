0f98c63170b18e5e655fc526e8a7a69afbf1ba01
An10nella <135998311+An10nella@users.noreply.github.com>
2023-08-09 20:26:25 +0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,15 @@ function twoSum (nums, target){
     
 
     for (let i in nums){
-        console.log(nums[i])
-    }
+        // console.log(nums[i]
+        for (let j in i){
+            // console.log(nums[j])
+            if(nums[i]+nums[j] == target){
+                return `${nums[i]} + ${nums[j]} = ${target}`
+            }
 
+        }
+    }
 }
+
+
